d40b7a67b666578d592309193ed5fc134476721b
Junius <luo.junius@gmail.com>
2017-05-21 12:35:57 -0700

    add service api file

---
+++ api/service/service.go
@@ -0,0 +1,48 @@
+package service
+
+const (
+	// The status of one service
+	// ServiceStatusCreating: creating the required resources of the service, such as volumes.
+	ServiceStatusCreating = "CREATING"
+	// ServiceStatusInitializing: service resources are created, wait for the initialization.
+	// at this state, the service containers are running. Some services require the special
+	// initialization configurations, such as configuring the DB replicaset. Some services
+	// does not require this step, such as Cassandra. The service creation script could simply
+	// change the service status to ACTIVE.
+	ServiceStatusInitializing = "INITIALIZING"
+	// ServiceStatusActive: service initialized and ready to use.
+	ServiceStatusActive   = "ACTIVE"
+	ServiceStatusDeleting = "DELETING"
+	ServiceStatusDeleted  = "DELETED"
+
+	// The status of one task
+	TaskStatusPending = "PENDING"
+	TaskStatusRunning = "RUNNING"
+	TaskStatusStopped = "STOPPED"
+)
+
+// Resources contains the service/task resources, cpu and memory.
+type Resources struct {
+	// The number of cpu units for the container.
+	// A container instance has 1,024 cpu units for every CPU core.
+	// MaxCPUUnits specifies how much of the available CPU resources a container can use.
+	// This will set docker's --cpu-period and --cpu-quota.
+	// ReserveCPUUnits is docker's --cpu-share, same with the ECS cpu units.
+	MaxCPUUnits     int64
+	ReserveCPUUnits int64
+	MaxMemMB        int64
+	ReserveMemMB    int64
+}
+
+type ServiceStatus struct {
+	RunningCount int64
+	DesiredCount int64
+}
+
+type TaskStatus struct {
+	// valid status: pending, running, stopped
+	Status        string
+	StoppedReason string
+	StartedAt     string // time format: 2017-05-20T19:50:56.834924582Z
+	FinishedAt    string
+}
