f097ad9631311c3c19429f5e01221c4bba24eb60
Junius <luo.junius@gmail.com>
2017-06-15 15:15:17 -0700

    initial postgres catalog service

---
+++ catalog/postgres/9.6/dockerfile/docker-entrypoint.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+set -e
+
+# the root db dir
+PGDIR=/data
+# the db data dir
+export PGDATA=$PGDIR/db
+# the target configs, these files will be copied to $PGDATA after initdb at primary or basebackup at standby
+PGConf=$PGDIR/postgresql.conf
+PGHbaConf=$PGDIR/pg_hba.conf
+PGRecoveryConf=$PGDIR/recovery.conf
+
+PGConfigFile=$PGDIR/sys.conf
+PGUSER="postgres"
+ROLE_PRIMARY="primary"
+ROLE_STANDBY="standby"
+
+
+SanityCheck() {
+  # sanity check to make sure the volume is mounted to $PGDATA.
+  if [ ! -d "$PGDIR" ]; then
+    echo "error: $PGDIR not exist. Please make sure the volume is mounted to $PGDIR." >&2
+    exit 1
+  fi
+
+  # sanity check to make sure the config file is created.
+  if [ ! -f "$PGConf" ] || [ ! -f "$PGHbaConf" ] || [ ! -f "$PGConfigFile" ]
+  then
+    echo "error: $PGConf or $PGHbaConf or $PGConfigFile not exist." >&2
+    exit 1
+  fi
+
+  # create the db data dir if not exist
+  if [ ! -d "$PGDATA" ]; then
+    mkdir $PGDATA
+    chmod 700 $PGDATA
+  fi
+
+  echo "PostgreSQL sanity check passes"
+}
+
+InitPrimaryDB() {
+  echo "PostgreSQL primary init process start"
+
+  # initialize db
+  eval "initdb -U $PGUSER -D $PGDATA"
+  echo "PostgreSQL primary initdb completes, initdb -U $PGUSER -D $PGDATA"
+
+  # copy over the config files
+  cp $PGConf $PGDATA/
+  cp $PGHbaConf $PGDATA/
+
+  # internal start of server to create replication user
+  pg_ctl -D "$PGDATA" -o "-c listen_addresses='localhost'" -w start
+
+  # The streaming replication is used. Could not use replication slot. If the standby goes down
+  # for a long time, the primary xlog will become full. The postgres db will be down.
+  # To tolerate a zone failure, the primary and standby would usually be deployed to 2 zones.
+  # If one AWS zone goes down for a while, the standby will be down as well.
+  # TODO archive to S3.
+
+  # create replication user
+  # if local auth method is not set as "trust" in pg_hba.conf, has to call with PGPASSWORD,
+  # such as PGPASSWORD=pass1234 psql -v ...
+  psql -v ON_ERROR_STOP=1 -U "$PGUSER" -c "CREATE ROLE "$REPLICATION_USER" WITH REPLICATION PASSWORD '$REPLICATION_PASSWORD' LOGIN"
+  echo "PostgreSQL replication user created, $REPLICATION_USER"
+
+  # stop postgres
+  pg_ctl -D "$PGDATA" -m fast -w stop
+
+  echo "PostgreSQL primary init process complete; ready for start up."
+}
+
+InitStandbyDB() {
+  echo "PostgreSQL standby init process start"
+
+  # create the .pgpass file under /home/postgres for the "postgres" user,
+  # so pg_basebackup does not require a password prompt.
+  # The .pgpass file contains lines of the following format:
+  #   hostname:port:database:username:password
+  echo "$PRIMARY_HOST:5432:replication:$REPLICATION_USER:$REPLICATION_PASSWORD" > ~/.pgpass
+  chmod 600 ~/.pgpass
+  echo "PostgreSQL created ~/.pgpass file"
+
+  pg_basebackup -h "$PRIMARY_HOST" -D "$PGDATA" -P -U "$REPLICATION_USER" --xlog-method=stream -w
+
+  # copy over the config files
+  cp $PGConf $PGDATA/
+  cp $PGHbaConf $PGDATA/
+  cp $PGRecoveryConf $PGDATA/
+
+  echo "PostgreSQL standby pg_basebackup from primary complete; ready for start up."
+}
+
+
+SanityCheck
+
+# allow the container to be started with `--user`
+if [ "$(id -u)" = '0' ]; then
+  pgdiruser=$(stat -c "%U" $PGDIR)
+  if [ "$pgdiruser" != "$PGUSER" ]; then
+  	echo "chown -R $PGUSER $PGDIR"
+  	chown -R $PGUSER "$PGDIR"
+  fi
+
+	mkdir -p /var/run/postgresql
+	chown -R postgres /var/run/postgresql
+	chmod g+s /var/run/postgresql
+
+	exec gosu postgres "$BASH_SOURCE" "$@"
+fi
+
+
+# load the configs from the config file, including the container role (primary or slave),
+# primary hostname, postgres password, replication user & password.
+. "$PGConfigFile"
+
+# check all required configs are loaded
+if [ -z "$CONTAINER_ROLE" ] || [ -z "$PRIMARY_HOST" ] || [ -z "$POSTGRES_PASSWORD" ] || [ -z "$REPLICATION_USER" ] || [ -z "$REPLICATION_PASSWORD" ]
+then
+  echo "error: please write all required configs in the config file $PGConfigFile." >&2
+  exit 1
+fi
+
+# chown if needed
+pgdirid=$(stat -c "%u" $PGDIR)
+if [ "$pgdirid" != "$(id -u)" ]; then
+  echo "chown -R $(id -u) $PGDIR"
+	chown -R "$(id -u)" "$PGDIR" 2>/dev/null || :
+fi
+
+# if PG_VERSION file does not exist, db is not initialized
+if [ ! -s "$PGDATA/PG_VERSION" ]; then
+  if [ "$CONTAINER_ROLE" = "$ROLE_PRIMARY" ]; then
+    InitPrimaryDB
+  else
+    InitStandbyDB
+  fi
+fi
+
+echo "$@"
+exec "$@"
---
+++ catalog/postgres/openmanage-aws-ecs-postgres-creation/main.go
@@ -0,0 +1,302 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/golang/glog"
+	"golang.org/x/net/context"
+
+	"github.com/cloudstax/openmanage/catalog"
+	"github.com/cloudstax/openmanage/common"
+	"github.com/cloudstax/openmanage/containersvc/awsecs"
+	"github.com/cloudstax/openmanage/dns"
+	"github.com/cloudstax/openmanage/manage"
+	"github.com/cloudstax/openmanage/server/awsec2"
+	"github.com/cloudstax/openmanage/utils"
+)
+
+// The script is a simple demo for how to create the postgres service, the ECS task definition and service.
+var (
+	service      = flag.String("service", "", "The target service name in ECS")
+	replicas     = flag.Int64("replicas", 1, "The number of replicas for the service")
+	volSizeGB    = flag.Int64("volume-size", 0, "The size of each EBS volume, unit: GB")
+	region       = flag.String("region", "", "The target AWS region")
+	cluster      = flag.String("cluster", "default", "The ECS cluster")
+	serverURL    = flag.String("server-url", "", "the management service url, default: "+dns.GetDefaultManageServiceURL("cluster", false))
+	tlsEnabled   = flag.Bool("tlsEnabled", false, "whether tls is enabled")
+	caFile       = flag.String("ca-file", "", "the ca file")
+	certFile     = flag.String("cert-file", "", "the cert file")
+	keyFile      = flag.String("key-file", "", "the key file")
+	cpuUnits     = flag.Int64("cpu-units", 128, "The number of cpu units to reserve for the container")
+	maxMemMB     = flag.Int64("max-memory", 128, "The hard limit for memory, unit: MB")
+	reserveMemMB = flag.Int64("soft-memory", 128, "The memory reserved for the container, unit: MB")
+	port         = flag.Int64("port", 5432, "The PostgreSQL listening port, default: 5432")
+	pgpasswd     = flag.String("passwd", "password", "The PostgreSQL DB password")
+	replUser     = flag.String("replication-user", "repluser", "The replication user that the standby DB replicates from the primary")
+	replPasswd   = flag.String("replication-passwd", "replpassword", "The password for the standby DB to access the primary")
+)
+
+const (
+	ContainerImage = common.ContainerNamePrefix + "postgres"
+	ContainerPath  = common.DefaultContainerMountPath
+
+	ContainerRolePrimary = "primary"
+	ContainerRoleStandby = "standby"
+	SysConfFileName      = "sys.conf"
+	PostgresConfFileName = "postgresql.conf"
+	PGHbaConfFileName    = "pg_hba.conf"
+	RecoveryConfFileName = "recovery.conf"
+)
+
+func usage() {
+	fmt.Println("usage: openmanage-aws-postgres-creation -op=create-service -region=us-west-1")
+}
+
+func main() {
+	flag.Parse()
+	defer glog.Flush()
+
+	if *service == "" || *replicas == 0 || *volSizeGB == 0 {
+		fmt.Println("please specify the valid service name, replicas, volume size")
+		os.Exit(-1)
+	}
+
+	mgtserverurl := *serverURL
+	if mgtserverurl == "" {
+		// use default server url
+		mgtserverurl = dns.GetDefaultManageServiceURL(*cluster, *tlsEnabled)
+	} else {
+		// format url
+		mgtserverurl = dns.FormatManageServiceURL(mgtserverurl, *tlsEnabled)
+	}
+
+	var err error
+	awsRegion := *region
+	if awsRegion == "" {
+		awsRegion, err = awsec2.GetLocalEc2Region()
+		if err != nil {
+			fmt.Println("please input the correct AWS region")
+			os.Exit(-1)
+		}
+	}
+
+	config := aws.NewConfig().WithRegion(awsRegion)
+	sess, err := session.NewSession(config)
+	if err != nil {
+		glog.Fatalln("failed to create aws session, error", err)
+	}
+
+	// make sure one replica per az
+	azs, err := awsec2.GetRegionAZs(awsRegion, sess)
+	if err != nil {
+		glog.Fatalln(err)
+	}
+	if len(azs) < int(*replicas) {
+		fmt.Println("region ", awsRegion, " only has ", len(azs), " availability zones")
+		os.Exit(-1)
+	}
+
+	ecsIns := awsecs.NewAWSEcs(sess)
+
+	sop, err := serviceop.NewServiceOp(ecsIns, awsRegion,
+		mgtserverurl, *tlsEnabled, *caFile, *certFile, *keyFile)
+	if err != nil {
+		glog.Fatalln(err)
+	}
+
+	ctx := context.Background()
+
+	// generate service ReplicaConfigs
+	replicaCfgs := genReplicaConfigs(*cluster, *service, azs, *replicas, *port, *pgpasswd, *replUser, *replPasswd)
+
+	req := &manage.CreateServiceRequest{
+		Service: &manage.ServiceCommonRequest{
+			Region:      awsRegion,
+			Cluster:     *cluster,
+			ServiceName: *service,
+		},
+
+		Resource: &common.Resources{
+			MaxCPUUnits:     *cpuUnits,
+			ReserveCPUUnits: *cpuUnits,
+			MaxMemMB:        *maxMemMB,
+			ReserveMemMB:    *reserveMemMB,
+		},
+
+		ContainerImage: ContainerImage,
+		Replicas:       *replicas,
+		VolumeSizeGB:   *volSizeGB,
+		ContainerPath:  ContainerPath,
+		Port:           *port,
+
+		HasMembership:  true,
+		ReplicaConfigs: replicaCfgs,
+	}
+
+	err = sop.CreateService(ctx, req, common.DefaultTaskWaitSeconds)
+	if err != nil {
+		glog.Fatalln("create service error", err, "cluster", *cluster, "service", *service)
+	}
+
+	// postgres does not require the special cluster membership setup. set the service initialized
+	err = sop.SetServiceInitialized(ctx, req.Service)
+	if err != nil {
+		glog.Fatalln("SetServiceInitialized error", err, req.Service)
+	}
+
+	fmt.Println("The PostgreSQL cluster are successfully initialized")
+}
+
+func genReplicaConfigs(cluster string, service string, azs []string, replicas int64, port int64, pgpasswd string, replUser string, replPasswd string) []*manage.ReplicaConfig {
+	replicaCfgs := make([]*manage.ReplicaConfig, replicas)
+
+	// generate the primary configs
+	domain := dns.GenDefaultDomainName(cluster)
+	primaryMember := utils.GenServiceMemberName(service, 0)
+	primaryHost := dns.GenDNSName(primaryMember, domain)
+	replicaCfgs[0] = genPrimaryConfig(azs[0], primaryHost, port, pgpasswd, replUser, replPasswd)
+
+	// generate the standby configs.
+	// TODO support cascading replication, specially for cross-region replication.
+	for i := int64(1); i < replicas; i++ {
+		replicaCfgs[i] = genStandbyConfig(azs[i], primaryHost, port, pgpasswd, replUser, replPasswd)
+	}
+
+	return replicaCfgs
+}
+
+func genPrimaryConfig(az string, primaryHost string, port int64, pgpasswd string,
+	replUser string, replPasswd string) *manage.ReplicaConfig {
+	// create sys.conf file
+	content := fmt.Sprintf(sysConf, ContainerRolePrimary, primaryHost, port, pgpasswd, replUser, replPasswd)
+	cfg1 := &manage.ReplicaConfigFile{FileName: SysConfFileName, Content: content}
+
+	// create postgres.conf
+	cfg2 := &manage.ReplicaConfigFile{FileName: PostgresConfFileName, Content: primaryPostgresConf}
+
+	// create pg_hba.conf
+	content = fmt.Sprintf(primaryPgHbaConf, replUser)
+	cfg3 := &manage.ReplicaConfigFile{FileName: PGHbaConfFileName, Content: content}
+
+	configs := []*manage.ReplicaConfigFile{cfg1, cfg2, cfg3}
+	return &manage.ReplicaConfig{Zone: az, Configs: configs}
+}
+
+func genStandbyConfig(az string, primaryHost string, port int64, pgpasswd string, replUser string, replPasswd string) *manage.ReplicaConfig {
+	// create sys.conf file
+	content := fmt.Sprintf(sysConf, ContainerRoleStandby, primaryHost, port, pgpasswd, replUser, replPasswd)
+	cfg1 := &manage.ReplicaConfigFile{FileName: SysConfFileName, Content: content}
+
+	// create postgres.conf
+	cfg2 := &manage.ReplicaConfigFile{FileName: PostgresConfFileName, Content: standbyPostgresConf}
+
+	// create pg_hba.conf
+	cfg3 := &manage.ReplicaConfigFile{FileName: PGHbaConfFileName, Content: standbyPgHbaConf}
+
+	// create recovery.conf
+	primaryConnInfo := fmt.Sprintf(standbyPrimaryConnInfo, primaryHost, port, replUser, replPasswd)
+	content = fmt.Sprintf(standbyRecoveryConf, primaryConnInfo, standbyRestoreCmd)
+	cfg4 := &manage.ReplicaConfigFile{FileName: RecoveryConfFileName, Content: content}
+
+	configs := []*manage.ReplicaConfigFile{cfg1, cfg2, cfg3, cfg4}
+	return &manage.ReplicaConfig{Zone: az, Configs: configs}
+}
+
+const (
+	sysConf = `
+CONTAINER_ROLE=%s
+PRIMARY_HOST=%s
+LISTEN_PORT=%d
+POSTGRES_PASSWORD=%s
+REPLICATION_USER=%s
+REPLICATION_PASSWORD=%s
+`
+
+	primaryPostgresConf = `
+listen_addresses = '*'
+
+# To enable read-only queries on a standby server, wal_level must be set to
+# "hot_standby".
+wal_level = hot_standby
+
+# allow up to 5 standby servers
+max_wal_senders = 5
+
+# To prevent the primary server from removing the WAL segments required for
+# the standby server before shipping them, set the minimum number of segments
+# retained in the pg_xlog directory. At least wal_keep_segments should be
+# larger than the number of segments generated between the beginning of
+# online-backup and the startup of streaming replication. If you enable WAL
+# archiving to an archive directory accessible from the standby, this may
+# not be necessary.
+wal_keep_segments = 32
+
+# Enable WAL archiving on the primary to an archive directory accessible from
+# the standby. If wal_keep_segments is a high enough number to retain the WAL
+# segments required for the standby server, this is not necessary.
+# TODO enable it with archiving to S3
+#archive_mode    = on
+#archive_command = 'cp %p /path_to/archive/%f'
+`
+
+	primaryPgHbaConf = `
+# TYPE  DATABASE        USER            ADDRESS                 METHOD
+
+# "local" is for Unix domain socket connections only
+local   all             all                                     trust
+
+# the application hosts to access PostgreSQL
+host    all             all               all                    md5
+
+# the PostgreSQL standby hosts to access PostgreSQL replication
+# need to allow all. The primary only gets the standby's ip address,
+# then does reverse lookup, which returns EC2's private DNS name.
+# The primary has no way to know the DNS name in Route53.
+# There is no security concern for this. The EC2's inbound rule will
+# limit the source from the specific security group.
+host   replication      %s                all                    md5
+`
+
+	standbyPostgresConf = `
+listen_addresses = '*'
+
+hot_standby = on
+`
+	standbyPgHbaConf = `
+# TYPE  DATABASE        USER            ADDRESS                 METHOD
+
+# "local" is for Unix domain socket connections only
+local   all             all                                     trust
+
+# the application hosts to access PostgreSQL
+host    all             all               all                    md5
+`
+
+	standbyPrimaryConnInfo = "host=%s port=%d user=%s password=%s"
+	standbyRestoreCmd      = `cp /path_to/archive/%f "%p"`
+	standbyRecoveryConf    = `
+# Specifies whether to start the server as a standby. In streaming replication,
+# this parameter must to be set to on.
+standby_mode          = 'on'
+
+# Specifies a connection string which is used for the standby server to connect
+# with the primary.
+primary_conninfo      = '%s'
+
+# Specifies a trigger file whose presence should cause streaming replication to
+# end (i.e., failover).
+trigger_file = '/data/recovery-trigger'
+
+# Specifies a command to load archive segments from the WAL archive. If
+# wal_keep_segments is a high enough number to retain the WAL segments
+# required for the standby server, this may not be necessary. But
+# a large workload can cause segments to be recycled before the standby
+# is fully synchronized, requiring you to start again from a new base backup.
+# TODO enable it with archiving to S3
+#restore_command = '%s'
+`
+)
