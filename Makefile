6acaa3addd00aac0be6d82fdcfe32ce08cb45f8b
Junius <luo.junius@gmail.com>
2017-11-17 19:15:18 -0800

    change release back to latest

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 .PHONY: build docker test sources rpm
 
-version=0.9
+version=latest
 
 install:
 	./scripts/install.sh
--- common/types.go
+++ common/types.go
@@ -1,7 +1,7 @@
 package common
 
 const (
-	Version = "0.9"
+	Version = "latest"
 
 	ContainerPlatformECS   = "ecs"
 	ContainerPlatformSwarm = "swarm"
