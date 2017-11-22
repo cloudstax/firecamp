b6111017a41475b10f9e4c8f24524a5e19f4d6b0
Junius <luo.junius@gmail.com>
2017-11-21 18:02:23 -0800

    update redis templates for quick start, zip copy

--- scripts/buildlambda.sh
+++ scripts/buildlambda.sh
@@ -4,7 +4,7 @@ set -xe
 export TOPWD="$(pwd)"
 cd ${TOPWD}/vendor/lambda-python-requests
 cp ${TOPWD}/packaging/aws-cloudformation/redis/redis.py .
-zip -r $GOPATH/bin/firecamp-lambda-redis.zip .
+zip -r $GOPATH/bin/redis-lambda.zip .
 rm -f redis.py
 cd -
 
