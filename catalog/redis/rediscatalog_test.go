6e651170185475e4be7d207189699efde8c5f907
Junius <luo.junius@gmail.com>
2017-11-24 23:43:50 -0800

    fix redis unit test

--- catalog/redis/rediscatalog_test.go
+++ catalog/redis/rediscatalog_test.go
@@ -36,19 +36,19 @@ func TestRedisConfigs(t *testing.T) {
 		t.Fatalf("expect 6 replica configs, get %d", len(replcfgs))
 	}
 
-	if replcfgs[0].Zone != azs[0] || replcfgs[0].MemberName != "service1-shard0-0" ||
-		replcfgs[1].Zone != azs[1] || replcfgs[1].MemberName != "service1-shard0-1" ||
-		replcfgs[2].Zone != azs[1] || replcfgs[2].MemberName != "service1-shard1-0" ||
-		replcfgs[3].Zone != azs[2] || replcfgs[3].MemberName != "service1-shard1-1" ||
-		replcfgs[4].Zone != azs[2] || replcfgs[4].MemberName != "service1-shard2-0" ||
-		replcfgs[5].Zone != azs[0] || replcfgs[5].MemberName != "service1-shard2-1" {
+	if replcfgs[0].Zone != azs[0] || replcfgs[0].MemberName != "service1-0" ||
+		replcfgs[1].Zone != azs[1] || replcfgs[1].MemberName != "service1-1" ||
+		replcfgs[2].Zone != azs[1] || replcfgs[2].MemberName != "service1-2" ||
+		replcfgs[3].Zone != azs[2] || replcfgs[3].MemberName != "service1-3" ||
+		replcfgs[4].Zone != azs[2] || replcfgs[4].MemberName != "service1-4" ||
+		replcfgs[5].Zone != azs[0] || replcfgs[5].MemberName != "service1-5" {
 		t.Fatalf("replica configs zone mismatch, %s", replcfgs)
 	}
 
 	content := strings.TrimSuffix(replcfgs[0].Configs[1].Content, defaultConfigs)
 	content = EnableRedisAuth(content)
 	content = SetClusterAnnounceIP(content, "ip1")
-	content1 := fmt.Sprintf(redisConfigs1, "service1-shard0-0.c1-firecamp.com")
+	content1 := fmt.Sprintf(redisConfigs1, "service1-0.c1-firecamp.com")
 	if content != content1 {
 		t.Fatalf("redis conf content mismatch, %s \nexpect %s", content, content1)
 	}
@@ -58,11 +58,11 @@ func TestRedisConfigs(t *testing.T) {
 	if len(envPairs) != 11 {
 		t.Fatalf("expect 11 env pairs, get %d", len(envPairs))
 	}
-	expectMasters := "service1-shard0-0.c1-firecamp.com,service1-shard1-0.c1-firecamp.com,service1-shard2-0.c1-firecamp.com"
+	expectMasters := "service1-0.c1-firecamp.com,service1-2.c1-firecamp.com,service1-4.c1-firecamp.com"
 	if envPairs[9].Name != envMasters || envPairs[9].Value != expectMasters {
 		t.Fatalf("expect masters %s, get %s", expectMasters, envPairs[9].Value)
 	}
-	expectSlaves := "service1-shard0-1.c1-firecamp.com,service1-shard1-1.c1-firecamp.com,service1-shard2-1.c1-firecamp.com"
+	expectSlaves := "service1-1.c1-firecamp.com,service1-3.c1-firecamp.com,service1-5.c1-firecamp.com"
 	if envPairs[10].Name != envSlaves || envPairs[10].Value != expectSlaves {
 		t.Fatalf("expect slaves %s, get %s", expectSlaves, envPairs[9].Value)
 	}
@@ -75,32 +75,32 @@ func TestRedisConfigs(t *testing.T) {
 		t.Fatalf("expect 12 replica configs, get %d", len(replcfgs))
 	}
 
-	if replcfgs[0].Zone != azs[0] || replcfgs[0].MemberName != "service1-shard0-0" ||
-		replcfgs[1].Zone != azs[1] || replcfgs[1].MemberName != "service1-shard0-1" ||
-		replcfgs[2].Zone != azs[2] || replcfgs[2].MemberName != "service1-shard0-2" ||
-		replcfgs[3].Zone != azs[1] || replcfgs[3].MemberName != "service1-shard1-0" ||
-		replcfgs[4].Zone != azs[2] || replcfgs[4].MemberName != "service1-shard1-1" ||
-		replcfgs[5].Zone != azs[0] || replcfgs[5].MemberName != "service1-shard1-2" ||
-		replcfgs[6].Zone != azs[2] || replcfgs[6].MemberName != "service1-shard2-0" ||
-		replcfgs[7].Zone != azs[0] || replcfgs[7].MemberName != "service1-shard2-1" ||
-		replcfgs[8].Zone != azs[1] || replcfgs[8].MemberName != "service1-shard2-2" ||
-		replcfgs[9].Zone != azs[0] || replcfgs[9].MemberName != "service1-shard3-0" ||
-		replcfgs[10].Zone != azs[1] || replcfgs[10].MemberName != "service1-shard3-1" ||
-		replcfgs[11].Zone != azs[2] || replcfgs[11].MemberName != "service1-shard3-2" {
+	if replcfgs[0].Zone != azs[0] || replcfgs[0].MemberName != "service1-0" ||
+		replcfgs[1].Zone != azs[1] || replcfgs[1].MemberName != "service1-1" ||
+		replcfgs[2].Zone != azs[2] || replcfgs[2].MemberName != "service1-2" ||
+		replcfgs[3].Zone != azs[1] || replcfgs[3].MemberName != "service1-3" ||
+		replcfgs[4].Zone != azs[2] || replcfgs[4].MemberName != "service1-4" ||
+		replcfgs[5].Zone != azs[0] || replcfgs[5].MemberName != "service1-5" ||
+		replcfgs[6].Zone != azs[2] || replcfgs[6].MemberName != "service1-6" ||
+		replcfgs[7].Zone != azs[0] || replcfgs[7].MemberName != "service1-7" ||
+		replcfgs[8].Zone != azs[1] || replcfgs[8].MemberName != "service1-8" ||
+		replcfgs[9].Zone != azs[0] || replcfgs[9].MemberName != "service1-9" ||
+		replcfgs[10].Zone != azs[1] || replcfgs[10].MemberName != "service1-10" ||
+		replcfgs[11].Zone != azs[2] || replcfgs[11].MemberName != "service1-11" {
 		t.Fatalf("replica configs zone mismatch, %s", replcfgs)
 	}
 
 	content = strings.TrimSuffix(replcfgs[1].Configs[1].Content, defaultConfigs)
 	content = EnableRedisAuth(content)
 	content = SetClusterAnnounceIP(content, "ip1")
-	content1 = fmt.Sprintf(redisConfigs1, "service1-shard0-1.c1-firecamp.com")
+	content1 = fmt.Sprintf(redisConfigs1, "service1-1.c1-firecamp.com")
 	if content != content1 {
 		t.Fatalf("redis conf content mismatch, %s \nexpect\n %s", content, content1)
 	}
 	content = strings.TrimSuffix(replcfgs[4].Configs[1].Content, defaultConfigs)
 	content = EnableRedisAuth(content)
 	content = SetClusterAnnounceIP(content, "ip1")
-	content1 = fmt.Sprintf(redisConfigs1, "service1-shard1-1.c1-firecamp.com")
+	content1 = fmt.Sprintf(redisConfigs1, "service1-4.c1-firecamp.com")
 	if content != content1 {
 		t.Fatalf("redis conf content mismatch, %s \nexpect\n %s", content, content1)
 	}
@@ -110,11 +110,11 @@ func TestRedisConfigs(t *testing.T) {
 	if len(envPairs) != 11 {
 		t.Fatalf("expect 11 env pairs, get %d", len(envPairs))
 	}
-	expectMasters = "service1-shard0-0.c1-firecamp.com,service1-shard1-0.c1-firecamp.com,service1-shard2-0.c1-firecamp.com,service1-shard3-0.c1-firecamp.com"
+	expectMasters = "service1-0.c1-firecamp.com,service1-3.c1-firecamp.com,service1-6.c1-firecamp.com,service1-9.c1-firecamp.com"
 	if envPairs[9].Name != envMasters || envPairs[9].Value != expectMasters {
 		t.Fatalf("expect masters %s, get %s", expectMasters, envPairs[9].Value)
 	}
-	expectSlaves = "service1-shard0-1.c1-firecamp.com,service1-shard0-2.c1-firecamp.com,service1-shard1-1.c1-firecamp.com,service1-shard1-2.c1-firecamp.com,service1-shard2-1.c1-firecamp.com,service1-shard2-2.c1-firecamp.com,service1-shard3-1.c1-firecamp.com,service1-shard3-2.c1-firecamp.com"
+	expectSlaves = "service1-1.c1-firecamp.com,service1-2.c1-firecamp.com,service1-4.c1-firecamp.com,service1-5.c1-firecamp.com,service1-7.c1-firecamp.com,service1-8.c1-firecamp.com,service1-10.c1-firecamp.com,service1-11.c1-firecamp.com"
 	if envPairs[10].Name != envSlaves || envPairs[10].Value != expectSlaves {
 		t.Fatalf("expect slaves %s, get %s", expectSlaves, envPairs[10].Value)
 	}
@@ -127,15 +127,15 @@ func TestRedisConfigs(t *testing.T) {
 		t.Fatalf("expect 3 replica configs, get %d", len(replcfgs))
 	}
 
-	if replcfgs[0].Zone != azs[0] || replcfgs[0].MemberName != "service1-shard0-0" ||
-		replcfgs[1].Zone != azs[1] || replcfgs[1].MemberName != "service1-shard0-1" ||
-		replcfgs[2].Zone != azs[2] || replcfgs[2].MemberName != "service1-shard0-2" {
+	if replcfgs[0].Zone != azs[0] || replcfgs[0].MemberName != "service1-0" ||
+		replcfgs[1].Zone != azs[1] || replcfgs[1].MemberName != "service1-1" ||
+		replcfgs[2].Zone != azs[2] || replcfgs[2].MemberName != "service1-2" {
 		t.Fatalf("replica configs zone mismatch, %s", replcfgs)
 	}
 
 	content = strings.TrimSuffix(replcfgs[1].Configs[1].Content, defaultConfigs)
 	content = EnableRedisAuth(content)
-	content1 = fmt.Sprintf(redisConfigs2, "service1-shard0-1.c1-firecamp.com", 268435456, "\nslaveof service1-shard0-0.c1-firecamp.com 6379")
+	content1 = fmt.Sprintf(redisConfigs2, "service1-1.c1-firecamp.com", 268435456, "\nslaveof service1-0.c1-firecamp.com 6379")
 	if content != content1 {
 		t.Fatalf("redis conf content mismatch, %s \nexpect\n %s", content, content1)
 	}
@@ -148,14 +148,14 @@ func TestRedisConfigs(t *testing.T) {
 		t.Fatalf("expect 1 replica configs, get %d", len(replcfgs))
 	}
 
-	if replcfgs[0].Zone != azs[0] || replcfgs[0].MemberName != "service1-shard0-0" {
+	if replcfgs[0].Zone != azs[0] || replcfgs[0].MemberName != "service1-0" {
 		t.Fatalf("replica configs zone mismatch, %s", replcfgs)
 	}
 
 	content = strings.TrimSuffix(replcfgs[0].Configs[1].Content, defaultConfigs)
 	content = EnableRedisAuth(content)
 	content += "\n"
-	content1 = fmt.Sprintf(redisConfigs2, "service1-shard0-0.c1-firecamp.com", 268435456, "")
+	content1 = fmt.Sprintf(redisConfigs2, "service1-0.c1-firecamp.com", 268435456, "")
 	if content != content1 {
 		t.Fatalf("redis conf content mismatch, %s \nexpect\n %s", content, content1)
 	}
