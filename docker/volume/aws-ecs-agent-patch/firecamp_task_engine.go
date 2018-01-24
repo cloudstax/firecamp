1d0034d7d2ee2d841bbb698b34e254ec09f333ec
Junius <luo.junius@gmail.com>
2018-01-24 10:30:56 -0800

    support firecamp log driver

--- docker/volume/aws-ecs-agent-patch/firecamp_task_engine.go
+++ docker/volume/aws-ecs-agent-patch/firecamp_task_engine.go
@@ -18,8 +18,13 @@ func (err *VolumeDriverConfigError) Error() string     { return err.msg }
 func (err *VolumeDriverConfigError) ErrorName() string { return "VolumeDriverConfigError" }
 
 // Define here again to avoid the dependency on githut.com/cloudstax/firecamp
-const volumeDriver = "cloudstax/firecamp-volume"
-const versionKey = "VERSION"
+const (
+	volumeDriver        = "cloudstax/firecamp-volume"
+	logDriver           = "cloudstax/firecamp-log"
+	logServiceUUIDKey   = "ServiceUUID"
+	logDriverMinVersion = "0.9.3"
+	versionKey          = "VERSION"
+)
 
 // Create a taskDefinition at AWS ECS console, created one volume, "Name" my-ecs-volume,
 // "Source Path" /test-volume. Then in "Container Definitions", create "Mount Points",
@@ -38,7 +43,7 @@ const versionKey = "VERSION"
 // belongs to the service registered to us.
 // update HostConfig with "-v volumename:/data --volume-driver=ourDriver"
 func AddVolumeDriver(hostConfig *docker.HostConfig, container *api.Container, cluster string, taskArn string, taskDef string) (newConfig *docker.HostConfig, err *VolumeDriverConfigError) {
-	if setVolumeDriver(hostConfig, cluster, taskArn, taskDef) {
+	if isFirecampService, serviceUUID := setVolumeDriver(hostConfig, cluster, taskArn, taskDef); isFirecampService {
 		// get the volume driver version from environment
 		version, ok := container.Environment[versionKey]
 		if !ok {
@@ -51,7 +56,17 @@ func AddVolumeDriver(hostConfig *docker.HostConfig, container *api.Container, cl
 		// set volume-driver in docker HostConfig
 		hostConfig.VolumeDriver = volumeDriver + ":" + version
 
-		seelog.Info("Add volume driver", hostConfig.VolumeDriver,
+		// set the log driver in docker HostConfig
+		if version >= logDriverMinVersion {
+			hostConfig.LogConfig = docker.LogConfig{
+				Type: logDriver + ":" + version,
+				Config: map[string]string{
+					logServiceUUIDKey: serviceUUID,
+				},
+			}
+		}
+
+		seelog.Info("add firecamp log driver", hostConfig.LogConfig, "and volume driver", hostConfig.VolumeDriver,
 			"to HostConfig Binds", hostConfig.Binds,
 			"cluster", cluster, "taskArn", taskArn, "taskDef", taskDef)
 	}
@@ -59,29 +74,31 @@ func AddVolumeDriver(hostConfig *docker.HostConfig, container *api.Container, cl
 	return hostConfig, nil
 }
 
-func setVolumeDriver(hostConfig *docker.HostConfig, cluster string, taskArn string, taskDef string) bool {
+func setVolumeDriver(hostConfig *docker.HostConfig, cluster string, taskArn string, taskDef string) (isFirecampService bool, serviceUUID string) {
 	if len(hostConfig.VolumesFrom) != 0 {
 		seelog.Info("task HostConfig has VolumeFrom, not a valid volume, cluster",
 			cluster, "taskArn", taskArn, "taskDef", taskDef)
-		return false
+		return false, ""
 	}
 
-	if len(hostConfig.Binds) == 0 || len(hostConfig.Binds) > 1 {
-		seelog.Info("HostConfig binds 0 volume or more than 1 volume2", hostConfig.Binds,
+	if len(hostConfig.Binds) == 0 || len(hostConfig.Binds) > 2 {
+		// some service, such as Cassandra, has 2 volumes. One for journal, one for data.
+		seelog.Info("HostConfig binds 0 volume or more than 2 volume2", hostConfig.Binds,
 			"cluster", cluster, "taskArn", taskArn, "taskDef", taskDef)
-		return false
+		return false, ""
 	}
 
 	// binds one volume, further check whether is valid volume.
 	// currently only support one volume per task
 
 	// check if HostConfig Binds is correct, should be ServiceUUID:/mountPathInContainer
+	// TODO we should add firecamp as prefix, such as firecamp-ServiceUUID:/mountPathInContainer
 	volSep := ":/"
 	s := strings.Split(hostConfig.Binds[0], volSep)
 	if len(s) != 2 {
 		seelog.Info("Bad HostConfig Binds", hostConfig.Binds,
 			"cluster", cluster, "taskArn", taskArn, "taskDef", taskDef)
-		return false
+		return false, ""
 	}
 
 	// TODO should we check if service is created in DB? For now, ECS allows non-path
@@ -89,10 +106,10 @@ func setVolumeDriver(hostConfig *docker.HostConfig, cluster string, taskArn stri
 	if strings.HasPrefix(s[0], "/") {
 		seelog.Info("HostConfig Binds is for local path", hostConfig.Binds,
 			"cluster", cluster, "taskArn", taskArn, "taskDef", taskDef)
-		return false
+		return false, ""
 	}
 
 	seelog.Info("HostConfig binds the valid volume", hostConfig.Binds,
 		"cluster", cluster, "taskArn", taskArn, "taskDef", taskDef)
-	return true
+	return true, s[0]
 }
