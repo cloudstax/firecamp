08cf3457f3115a3a23461d43c8508cc962aae407
Junius <luo.junius@gmail.com>
2018-02-15 09:56:34 -0800

    revert the firecamp prefix in the log group name

--- log/utils.go
+++ log/utils.go
@@ -2,16 +2,14 @@ package cloudlog
 
 import (
 	"fmt"
-
-	"github.com/cloudstax/firecamp/common"
 )
 
-// GenServiceLogGroupName creates the service log group name: firecamp-clustername-servicename-serviceUUID.
+// GenServiceLogGroupName creates the service log group name: clustername-servicename-serviceUUID.
 func GenServiceLogGroupName(cluster string, service string, serviceUUID string, k8snamespace string) string {
 	if len(k8snamespace) != 0 {
-		return fmt.Sprintf("%s-%s-%s-%s-%s", common.SystemName, cluster, k8snamespace, service, serviceUUID)
+		return fmt.Sprintf("%s-%s-%s-%s", cluster, k8snamespace, service, serviceUUID)
 	}
-	return fmt.Sprintf("%s-%s-%s-%s", common.SystemName, cluster, service, serviceUUID)
+	return fmt.Sprintf("%s-%s-%s", cluster, service, serviceUUID)
 }
 
 // GenServiceMemberLogStreamName creates the log stream name for one service member: membername/hostname/containerID.
