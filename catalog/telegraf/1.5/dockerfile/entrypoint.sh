889007621037d311f0b23bb9eb04c19ee9c48936
Junius <luo.junius@gmail.com>
2018-03-11 16:28:31 -0700

    add more telegraf input plugin confs

--- catalog/telegraf/1.5/dockerfile/entrypoint.sh
+++ catalog/telegraf/1.5/dockerfile/entrypoint.sh
@@ -2,23 +2,35 @@
 set -e
 
 # check required parameters
-if [ -z "$REGION" -o -z "$CLUSTER" -o -z "$SERVICE_NAME" -o -z "$SERVICE_TYPE" ]; then
-  echo "error: please specify the REGION $REGION, CLUSTER $CLUSTER, SERVICE_NAME $SERVICE_NAME, SERVICE_TYPE $SERVICE_TYPE"
+# SERVICE_NAME is the stateful service to monitor.
+# TELEGRAF_SERVICE_NAME is the telegraf service that monitors the stateful service.
+if [ -z "$REGION" -o -z "$CLUSTER" -o -z "$SERVICE_NAME" -o -z "$SERVICE_TYPE" -o -z "$SERVICE_MEMBERS" -o -z "$TELEGRAF_SERVICE_NAME" ]; then
+  echo "error: please specify the REGION $REGION, CLUSTER $CLUSTER, SERVICE_NAME $SERVICE_NAME, SERVICE_TYPE $SERVICE_TYPE, SERVICE_MEMBERS $SERVICE_MEMBERS, TELEGRAF_SERVICE_NAME $TELEGRAF_SERVICE_NAME"
   exit 1
 fi
 
+# set telegraf configs
+export HOSTNAME=$TELEGRAF_SERVICE_NAME
+export INTERVAL="60s"
+if [ -z "$COLLECT_INTERVAL" ]; then
+  export INTERVAL=$COLLECT_INTERVAL
+fi
+
+# get service members array
+OIFS=$IFS
+IFS=','
+read -a members <<< "${SERVICE_MEMBERS}"
+IFS=$OIFS
+
+
 # add input plugin
 if [ "$SERVICE_TYPE" = "redis" ]; then
-  # check redis required parameters
+  # check the service required parameters
   # TODO simply pass redis auth password in the env variable. should fetch from DB or manage server.
-  if [ -z "$SERVICE_MEMBERS" -o -z "$REDIS_AUTH" ]; then
-    echo "error: please specify SERVICE_MEMBERS $SERVICE_MEMBERS and REDIS_AUTH $REDIS_AUTH"
+  if [ -z "$REDIS_AUTH" ]; then
+    echo "error: please specify REDIS_AUTH $REDIS_AUTH"
+    exit 2
   fi
-
-  OIFS=$IFS
-  IFS=','
-  read -a members <<< "${SERVICE_MEMBERS}"
-  IFS=$OIFS
 
   servers=""
   i=0
@@ -31,12 +43,33 @@ if [ "$SERVICE_TYPE" = "redis" ]; then
     i=$(( $i + 1 ))
   done
 
-  # update the redis servers to input_redis.conf
-  sed -i "s/\"firecamp-redis-servers\"/$servers/g" /firecamp/input_redis.conf
+  # update the servers in input conf
+  sed -i "s/\"firecamp-service-servers\"/$servers/g" /firecamp/input_redis.conf
 
-  # add redis input plugin to telegraf.conf
+  # add service input plugin to telegraf.conf
   cat /firecamp/input_redis.conf >> /firecamp/telegraf.conf
 fi
+
+# add input plugin
+if [ "$SERVICE_TYPE" = "zookeeper" ]; then
+  servers=""
+  i=0
+  for m in "${members[@]}"; do
+    if [ "$i" = "0" ]; then
+      servers="\"$m\""
+    else
+      servers+=",\"$m\""
+    fi
+    i=$(( $i + 1 ))
+  done
+
+  # update the servers in input conf
+  sed -i "s/\"firecamp-service-servers\"/$servers/g" /firecamp/input_zk.conf
+
+  # add service input plugin to telegraf.conf
+  cat /firecamp/input_zk.conf >> /firecamp/telegraf.conf
+fi
+
 
 # add output plugin
 cat /firecamp/output_cloudwatch.conf >> /firecamp/telegraf.conf
